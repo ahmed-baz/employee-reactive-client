66459e236eaa7cb31a3de464279314105da6783c
Ahmed Baz <developer.baz@gmail.com>
2024-02-14 11:28:36 +0200

    update getRandomList with NewEmployeeException

---
+++ src/main/java/org/demo/app/exception/NewEmployeeException.java
@@ -0,0 +1,8 @@
+package org.demo.app.exception;
+
+public class NewEmployeeException extends RuntimeException {
+
+    public NewEmployeeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/demo/app/proxy/EmployeeProxy.java
+++ src/main/java/org/demo/app/proxy/EmployeeProxy.java
@@ -2,8 +2,10 @@ package org.demo.app.proxy;
 
 
 import lombok.RequiredArgsConstructor;
-import org.demo.app.exception.EmployeeReactiveException;
+import lombok.extern.log4j.Log4j2;
+import org.demo.app.exception.NewEmployeeException;
 import org.demo.app.model.Employee;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
@@ -16,6 +18,7 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class EmployeeProxy {
 
     private final WebClient webClient;
@@ -33,12 +36,19 @@ public class EmployeeProxy {
     }
 
     public Flux<Employee> getRandomList(int size) {
+        DateTime last3Months = new DateTime().minusMonths(3);
         return webClient
                 .get()
                 .uri(FIND_RANDOM_LIST_PATH + size)
                 .exchangeToFlux(response -> response.bodyToFlux(Employee.class))
-                //.onErrorResume(e -> Flux.just(getStaticEmployee()));
-                .onErrorResume(e -> e instanceof WebClientRequestException, e -> Flux.just(getStaticEmployee()));
+                .onErrorResume(e -> e instanceof WebClientRequestException, e -> Flux.just(getStaticEmployee()))
+                .doOnNext(employee -> {
+                    if (employee.getJoinDate().compareTo(last3Months.toDate()) >= 0) {
+                        throw new NewEmployeeException("new employee joined recently in last 3 months");
+                    }
+                })
+                .onErrorContinue(NewEmployeeException.class,
+                        (e, o) -> log.error("{} with id {} and join date {}", e.getMessage(), ((Employee) o).getId(), ((Employee) o).getJoinDate()));
     }
 
     public Mono<Employee> getOne(int id) {
